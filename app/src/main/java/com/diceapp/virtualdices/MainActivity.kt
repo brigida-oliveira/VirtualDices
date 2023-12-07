711da217b67c0eeffd5ec349506869190f5e6efb
Brígida Oliveira <ocfb0914@hotmail.com>
2023-12-07 09:39:11 -0300

    Atualização

--- app/src/main/java/com/diceapp/virtualdices/MainActivity.kt
+++ app/src/main/java/com/diceapp/virtualdices/MainActivity.kt
@@ -29,7 +29,6 @@ class MainActivity : AppCompatActivity() {
         diceButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
 
             if (isChecked) {
-                val diceImage : ImageView = findViewById(R.id.dice)
                 when (checkedId) {
                     R.id.d4 -> {
                         //diceImage.setImageResource(R.drawable.ic_d4_1)
@@ -65,7 +64,7 @@ class MainActivity : AppCompatActivity() {
             } else {
                 if (toggleButtonGroup.checkedButtonId == View.NO_ID) {
                     // Do a dice roll when the app starts
-                    Toast.makeText(this, "Selecione uma opção", Toast.LENGTH_SHORT)
+                    Toast.makeText(this, "Selecione uma opção", Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -86,83 +85,90 @@ class MainActivity : AppCompatActivity() {
         val diceImage : ImageView = findViewById(R.id.dice)
 
         // Determine which drawable resource ID to use based on the dice roll
-        if(sides == 4){
-            when (diceRoll) {
-                1 -> diceImage.setImageResource(R.drawable.ic_d4_1)
-                2 -> diceImage.setImageResource(R.drawable.ic_d4_2)
-                3 -> diceImage.setImageResource(R.drawable.ic_d4_3)
-                else -> diceImage.setImageResource(R.drawable.ic_d4_4)
+        when (sides) {
+            4 -> {
+                when (diceRoll) {
+                    1 -> diceImage.setImageResource(R.drawable.ic_d4_1)
+                    2 -> diceImage.setImageResource(R.drawable.ic_d4_2)
+                    3 -> diceImage.setImageResource(R.drawable.ic_d4_3)
+                    else -> diceImage.setImageResource(R.drawable.ic_d4_4)
+                }
             }
-        } else if (sides == 6) {
-            when (diceRoll) {
-                1 -> diceImage.setImageResource(R.drawable.ic_dice1)
-                2 -> diceImage.setImageResource(R.drawable.ic_dice2)
-                3 -> diceImage.setImageResource(R.drawable.ic_dice3)
-                4 -> diceImage.setImageResource(R.drawable.ic_dice4)
-                5 -> diceImage.setImageResource(R.drawable.ic_dice5)
-                else -> diceImage.setImageResource(R.drawable.ic_dice6)
+            6 -> {
+                when (diceRoll) {
+                    1 -> diceImage.setImageResource(R.drawable.ic_dice1)
+                    2 -> diceImage.setImageResource(R.drawable.ic_dice2)
+                    3 -> diceImage.setImageResource(R.drawable.ic_dice3)
+                    4 -> diceImage.setImageResource(R.drawable.ic_dice4)
+                    5 -> diceImage.setImageResource(R.drawable.ic_dice5)
+                    else -> diceImage.setImageResource(R.drawable.ic_dice6)
+                }
             }
-        } else if (sides == 8) {
-            when (diceRoll) {
-                1 -> diceImage.setImageResource(R.drawable.ic_d8_1)
-                2 -> diceImage.setImageResource(R.drawable.ic_d8_2)
-                3 -> diceImage.setImageResource(R.drawable.ic_d8_3)
-                4 -> diceImage.setImageResource(R.drawable.ic_d8_4)
-                5 -> diceImage.setImageResource(R.drawable.ic_d8_5)
-                6 -> diceImage.setImageResource(R.drawable.ic_d8_6)
-                7 -> diceImage.setImageResource(R.drawable.ic_d8_7)
-                else -> diceImage.setImageResource(R.drawable.ic_d8_8)
+            8 -> {
+                when (diceRoll) {
+                    1 -> diceImage.setImageResource(R.drawable.ic_d8_1)
+                    2 -> diceImage.setImageResource(R.drawable.ic_d8_2)
+                    3 -> diceImage.setImageResource(R.drawable.ic_d8_3)
+                    4 -> diceImage.setImageResource(R.drawable.ic_d8_4)
+                    5 -> diceImage.setImageResource(R.drawable.ic_d8_5)
+                    6 -> diceImage.setImageResource(R.drawable.ic_d8_6)
+                    7 -> diceImage.setImageResource(R.drawable.ic_d8_7)
+                    else -> diceImage.setImageResource(R.drawable.ic_d8_8)
+                }
             }
-        } else if (sides == 10) {
-            when (diceRoll) {
-                1 -> diceImage.setImageResource(R.drawable.ic_d10_1)
-                2 -> diceImage.setImageResource(R.drawable.ic_d10_2)
-                3 -> diceImage.setImageResource(R.drawable.ic_d10_3)
-                4 -> diceImage.setImageResource(R.drawable.ic_d10_4)
-                5 -> diceImage.setImageResource(R.drawable.ic_d10_5)
-                6 -> diceImage.setImageResource(R.drawable.ic_d10_6)
-                7 -> diceImage.setImageResource(R.drawable.ic_d10_7)
-                8 -> diceImage.setImageResource(R.drawable.ic_d10_8)
-                9 -> diceImage.setImageResource(R.drawable.ic_d10_9)
-                else -> diceImage.setImageResource(R.drawable.ic_d10_10)
+            10 -> {
+                when (diceRoll) {
+                    1 -> diceImage.setImageResource(R.drawable.ic_d10_1)
+                    2 -> diceImage.setImageResource(R.drawable.ic_d10_2)
+                    3 -> diceImage.setImageResource(R.drawable.ic_d10_3)
+                    4 -> diceImage.setImageResource(R.drawable.ic_d10_4)
+                    5 -> diceImage.setImageResource(R.drawable.ic_d10_5)
+                    6 -> diceImage.setImageResource(R.drawable.ic_d10_6)
+                    7 -> diceImage.setImageResource(R.drawable.ic_d10_7)
+                    8 -> diceImage.setImageResource(R.drawable.ic_d10_8)
+                    9 -> diceImage.setImageResource(R.drawable.ic_d10_9)
+                    else -> diceImage.setImageResource(R.drawable.ic_d10_10)
+                }
             }
-        } else if (sides == 12) {
-            when (diceRoll) {
-                1 -> diceImage.setImageResource(R.drawable.ic_d12_1)
-                2 -> diceImage.setImageResource(R.drawable.ic_d12_2)
-                3 -> diceImage.setImageResource(R.drawable.ic_d12_3)
-                4 -> diceImage.setImageResource(R.drawable.ic_d12_4)
-                5 -> diceImage.setImageResource(R.drawable.ic_d12_5)
-                6 -> diceImage.setImageResource(R.drawable.ic_d12_6)
-                7 -> diceImage.setImageResource(R.drawable.ic_d12_7)
-                8 -> diceImage.setImageResource(R.drawable.ic_d12_8)
-                9 -> diceImage.setImageResource(R.drawable.ic_d12_9)
-                10 -> diceImage.setImageResource(R.drawable.ic_d12_10)
-                11 -> diceImage.setImageResource(R.drawable.ic_d12_11)
-                else -> diceImage.setImageResource(R.drawable.ic_d12_12)
+            12 -> {
+                when (diceRoll) {
+                    1 -> diceImage.setImageResource(R.drawable.ic_d12_1)
+                    2 -> diceImage.setImageResource(R.drawable.ic_d12_2)
+                    3 -> diceImage.setImageResource(R.drawable.ic_d12_3)
+                    4 -> diceImage.setImageResource(R.drawable.ic_d12_4)
+                    5 -> diceImage.setImageResource(R.drawable.ic_d12_5)
+                    6 -> diceImage.setImageResource(R.drawable.ic_d12_6)
+                    7 -> diceImage.setImageResource(R.drawable.ic_d12_7)
+                    8 -> diceImage.setImageResource(R.drawable.ic_d12_8)
+                    9 -> diceImage.setImageResource(R.drawable.ic_d12_9)
+                    10 -> diceImage.setImageResource(R.drawable.ic_d12_10)
+                    11 -> diceImage.setImageResource(R.drawable.ic_d12_11)
+                    else -> diceImage.setImageResource(R.drawable.ic_d12_12)
+                }
             }
-        } else {
-            when (diceRoll) {
-                1 -> diceImage.setImageResource(R.drawable.ic_d20_1)
-                2 -> diceImage.setImageResource(R.drawable.ic_d20_2)
-                3 -> diceImage.setImageResource(R.drawable.ic_d20_3)
-                4 -> diceImage.setImageResource(R.drawable.ic_d20_4)
-                5 -> diceImage.setImageResource(R.drawable.ic_d20_5)
-                6 -> diceImage.setImageResource(R.drawable.ic_d20_6)
-                7 -> diceImage.setImageResource(R.drawable.ic_d20_7)
-                8 -> diceImage.setImageResource(R.drawable.ic_d20_8)
-                9 -> diceImage.setImageResource(R.drawable.ic_d20_9)
-                10 -> diceImage.setImageResource(R.drawable.ic_d20_10)
-                11 -> diceImage.setImageResource(R.drawable.ic_d20_11)
-                12 -> diceImage.setImageResource(R.drawable.ic_d20_12)
-                13 -> diceImage.setImageResource(R.drawable.ic_d20_13)
-                14 -> diceImage.setImageResource(R.drawable.ic_d20_14)
-                15 -> diceImage.setImageResource(R.drawable.ic_d20_15)
-                16 -> diceImage.setImageResource(R.drawable.ic_d20_16)
-                17 -> diceImage.setImageResource(R.drawable.ic_d20_17)
-                18 -> diceImage.setImageResource(R.drawable.ic_d20_18)
-                19 -> diceImage.setImageResource(R.drawable.ic_d20_19)
-                else -> diceImage.setImageResource(R.drawable.ic_d20_20)
+            else -> {
+                when (diceRoll) {
+                    1 -> diceImage.setImageResource(R.drawable.ic_d20_1)
+                    2 -> diceImage.setImageResource(R.drawable.ic_d20_2)
+                    3 -> diceImage.setImageResource(R.drawable.ic_d20_3)
+                    4 -> diceImage.setImageResource(R.drawable.ic_d20_4)
+                    5 -> diceImage.setImageResource(R.drawable.ic_d20_5)
+                    6 -> diceImage.setImageResource(R.drawable.ic_d20_6)
+                    7 -> diceImage.setImageResource(R.drawable.ic_d20_7)
+                    8 -> diceImage.setImageResource(R.drawable.ic_d20_8)
+                    9 -> diceImage.setImageResource(R.drawable.ic_d20_9)
+                    10 -> diceImage.setImageResource(R.drawable.ic_d20_10)
+                    11 -> diceImage.setImageResource(R.drawable.ic_d20_11)
+                    12 -> diceImage.setImageResource(R.drawable.ic_d20_12)
+                    13 -> diceImage.setImageResource(R.drawable.ic_d20_13)
+                    14 -> diceImage.setImageResource(R.drawable.ic_d20_14)
+                    15 -> diceImage.setImageResource(R.drawable.ic_d20_15)
+                    16 -> diceImage.setImageResource(R.drawable.ic_d20_16)
+                    17 -> diceImage.setImageResource(R.drawable.ic_d20_17)
+                    18 -> diceImage.setImageResource(R.drawable.ic_d20_18)
+                    19 -> diceImage.setImageResource(R.drawable.ic_d20_19)
+                    else -> diceImage.setImageResource(R.drawable.ic_d20_20)
+                }
             }
         }
 
